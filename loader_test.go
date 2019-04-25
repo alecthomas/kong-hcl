19e28f143af81c012ec01764a088f6a4480e3a30
Alec Thomas <alec@swapoff.org>
2019-04-25 16:27:28 +1000

    Tests for fragment loading from flag/file.

--- loader_test.go
+++ loader_test.go
@@ -2,12 +2,39 @@
 package konghcl
 
 import (
+	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 
 	"github.com/alecthomas/kong"
 	"github.com/stretchr/testify/require"
 )
+
+const testConfig = `
+	flag-name = "hello world"
+	int-flag = 10
+	float-flag = 10.5
+	slice-flag = [1, 2, 3]
+
+	prefix {
+		prefixed-flag = "prefixed flag"
+	}
+	group {
+		grouped-flag = "grouped flag"
+		embedded-flag = "embedded flag"
+	}
+	map-flag = {
+		key = "value"
+	}
+	// Multiple keys are merged.
+	mapped = {
+		left = "left"
+	}
+	mapped = {
+		right = "right"
+	}
+`
 
 type mapperValue struct {
 	Left  string
@@ -22,7 +49,7 @@ func TestHCL(t *testing.T) {
 	type Embedded struct {
 		EmbeddedFlag string
 	}
-	var cli struct {
+	type CLI struct {
 		FlagName     string
 		IntFlag      int
 		FloatFlag    float64
@@ -33,45 +60,54 @@ func TestHCL(t *testing.T) {
 		MapFlag      map[string]string
 		Mapped       mapperValue
 	}
-	r := strings.NewReader(`
-		flag-name = "hello world"
-		int-flag = 10
-		float-flag = 10.5
-		slice-flag = [1, 2, 3]
 
-		prefix {
-			prefixed-flag = "prefixed flag"
-		}
-		group {
-			grouped-flag = "grouped flag"
-			embedded-flag = "embedded flag"
-		}
-		map-flag = {
-			key = "value"
-		}
-		// Multiple keys are merged.
-		mapped = {
-			left = "left"
-		}
-		mapped = {
-			right = "right"
-		}
-	`)
-	resolver, err := Loader(r)
-	require.NoError(t, err)
-	parser, err := kong.New(&cli, kong.Resolvers(resolver))
-	require.NoError(t, err)
-	_, err = parser.Parse(nil)
-	require.NoError(t, err)
-	require.Equal(t, "hello world", cli.FlagName)
-	require.Equal(t, "grouped flag", cli.GroupedFlag)
-	require.Equal(t, "prefixed flag", cli.PrefixedFlag)
-	require.Equal(t, "embedded flag", cli.EmbeddedFlag)
-	require.Equal(t, 10, cli.IntFlag)
-	require.Equal(t, 10.5, cli.FloatFlag)
-	require.Equal(t, []int{1, 2, 3}, cli.SliceFlag)
-	require.Equal(t, map[string]string{"key": "value"}, cli.MapFlag)
-	require.Equal(t, mapperValue{Left: "left", Right: "right"}, cli.Mapped)
+	t.Run("FromResolver", func(t *testing.T) {
+		var cli CLI
+		r := strings.NewReader(testConfig)
+		resolver, err := Loader(r)
+		require.NoError(t, err)
+		parser, err := kong.New(&cli, kong.Resolvers(resolver))
+		require.NoError(t, err)
+		_, err = parser.Parse(nil)
+		require.NoError(t, err)
+		require.Equal(t, "hello world", cli.FlagName)
+		require.Equal(t, "grouped flag", cli.GroupedFlag)
+		require.Equal(t, "prefixed flag", cli.PrefixedFlag)
+		require.Equal(t, "embedded flag", cli.EmbeddedFlag)
+		require.Equal(t, 10, cli.IntFlag)
+		require.Equal(t, 10.5, cli.FloatFlag)
+		require.Equal(t, []int{1, 2, 3}, cli.SliceFlag)
+		require.Equal(t, map[string]string{"key": "value"}, cli.MapFlag)
+		require.Equal(t, mapperValue{Left: "left", Right: "right"}, cli.Mapped)
+	})
+
+	t.Run("FragmentFromFlag", func(t *testing.T) {
+		var cli CLI
+		parser, err := kong.New(&cli)
+		require.NoError(t, err)
+		_, err = parser.Parse([]string{"--mapped", `{"left":"LEFT","right":"RIGHT"}`})
+		require.NoError(t, err)
+		require.Equal(t, mapperValue{Left: "LEFT", Right: "RIGHT"}, cli.Mapped)
+	})
+
+	t.Run("FragmentFromFile", func(t *testing.T) {
+		w, err := ioutil.TempFile("", "kong-hcl-")
+		require.NoError(t, err)
+		_, err = w.Write([]byte(`
+		left = "LEFT"
+		right = "RIGHT"
+		`))
+		require.NoError(t, err)
+		_ = w.Close()
+		defer os.Remove(w.Name())
+
+		var cli CLI
+		parser, err := kong.New(&cli)
+		require.NoError(t, err)
+		_, err = parser.Parse([]string{"--mapped", w.Name()})
+		require.NoError(t, err)
+		require.Equal(t, mapperValue{Left: "LEFT", Right: "RIGHT"}, cli.Mapped)
+	})
 }
 
 func TestHCLValidation(t *testing.T) {
